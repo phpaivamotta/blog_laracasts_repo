404014ad63e6320d5a7e0c91065285abb10d0656
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-06-10 12:21:27 -0300

    fixed redirect from post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Session;
 
 class PostController extends Controller
 {
 
-    // // this controller action gets posts (either for the home page as normal, or for the search results)
+    //this controller action gets posts (either for the home page as normal, or for the search results)
     public function index()
     {   
+        // store current session url so that it can be used in the 'Back to Posts' link in the posts.show view
+        Session::put('blog_url', request()->fullUrl());
+        
         return view('posts.index', [
             'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(6)->withQueryString()
         ]);

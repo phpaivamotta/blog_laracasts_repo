60dd78e2700a7c64559689e2a9a2e910ea5039b6
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-06-25 14:03:20 -0300

    added ancho tag to posts thumbnail

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -1,9 +1,10 @@
 @props(['post'])
-<article
-    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+<article class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
     <div class="py-6 px-5 lg:flex">
         <div class="flex-1 lg:mr-8">
-            <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="Blog Post illustration" class="rounded-xl">
+            <a href="/posts/{{$post->slug}}">
+                <img src="{{ asset('storage/' . $post->thumbnail) }}" alt="Blog Post illustration" class="rounded-xl">
+            </a>
         </div>
 
         <div class="flex-1 flex flex-col justify-between">
@@ -20,13 +21,13 @@
                     </h1>
 
                     <span class="mt-2 block text-gray-400 text-xs">
-                            Publicado <time>{{$post->created_at->diffForHumans()}}</time>
-                        </span>
+                        Publicado <time>{{$post->created_at->diffForHumans()}}</time>
+                    </span>
                 </div>
             </header>
 
             <div class="text-sm mt-2 space-y-4">
-                    {!! $post->excerpt !!}
+                {!! $post->excerpt !!}
             </div>
 
             <footer class="flex justify-between items-center mt-8">
@@ -44,9 +45,7 @@
                 </div>
 
                 <div class="hidden lg:block">
-                    <a href="/posts/{{$post->slug}}"
-                        class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
-                    >Leia Mais</a>
+                    <a href="/posts/{{$post->slug}}" class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8">Leia Mais</a>
                 </div>
             </footer>
         </div>

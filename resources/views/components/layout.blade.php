0ad4900b327dc1c6af5a019c25d93f146367de00
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-07-06 22:34:10 -0300

    took-off html smooth scroll behavior

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,12 +9,6 @@
 <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
 
 
-<style>
-    html {
-        scroll-behavior: smooth;
-    }
-</style>
-
 <body style="font-family: Open Sans, sans-serif">
     <section class="px-6 py-8">
         <nav class="md:flex md:justify-between md:items-center">

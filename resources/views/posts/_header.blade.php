09e764e4792e24542c84e3869dff119ca63f3d41
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-07-13 19:41:06 -0300

    fixed size display ploblem on smaller devices by removing lg: from inline-flex _header.blade.php file

--- resources/views/posts/_header.blade.php
+++ resources/views/posts/_header.blade.php
@@ -5,7 +5,7 @@
     
     <div class="space-y-2 lg:space-y-0 lg:space-x-4 mt-4">
         <!-- Search -->
-        <div class="relative flex lg:inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
+        <div class="relative flex inline-flex items-center bg-gray-100 rounded-xl px-3 py-2">
             <form method="GET" action="/">
 
                 <input type="text" 

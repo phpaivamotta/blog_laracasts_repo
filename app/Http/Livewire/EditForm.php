50a0000bea69ddb649421442aeced2a8e6d136f8
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-07-18 18:32:37 -0300

    the profile/edit view is now a livewire component (edit-form)

---
+++ app/Http/Livewire/EditForm.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\User;
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class EditForm extends Component
+{
+    use WithFileUploads;
+
+    public $user;
+    public $name;
+    public $username;
+    public $profile_pic;
+    public $profilePicId;
+    public $tempUrl;
+
+    protected function rules()
+    {
+        // check to see if $profile_pic is a string because if it is, then it has already been stored into the storage public/storage folder
+        if (is_string($this->profile_pic)) {
+            return [
+                'name' => ['required', 'max:255'],
+                'username' => ['required', 'min:3', 'max:255', 'unique:users,username,' . $this->user->id]
+            ];
+        } else {
+            return [
+                'name' => ['required', 'max:255'],
+                'username' => ['required', 'min:3', 'max:255', 'unique:users,username,' . $this->user->id],
+                'profile_pic' => ['image', 'nullable']
+            ];
+        }
+    }
+
+    protected $validationAttributes = [
+        'profile_pic' => 'foto de perfil'
+    ];
+
+    public function mount(User $user)
+    {
+        $this->name = $user->name;
+        $this->username = $user->username;
+        $this->profile_pic = $user->profile_pic;
+    }
+
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+
+    public function updatedProfile_pic()
+    {
+        $this->validate();
+    }
+
+    public function removeSelectedProfilePic()
+    {
+        $this->profile_pic = null;
+        $this->profilePicId++;
+    }
+
+    public function update()
+    {
+        // validate user data submitted and store them in the variable $attributes
+        $attributes = $this->validate();
+
+        // check to see if a profile picture was added
+        // if so, store profile_pic in profile_pics folder storage>app>public>profile_pics
+        // otherwise, set profile pic to generic picture in public>images
+        if (isset($attributes['profile_pic'])) {
+            $attributes['profile_pic'] = $this->profile_pic->store('profile_pics');
+        }
+
+        // update the user with validated attributes
+        $this->user->update($attributes);
+
+        // redirect user to homepage after creating a profile
+        // and flash a message saying account has been created successfully
+        return redirect('/')->with('success', 'Seu perfil foi atualizado!');
+    }
+
+    public function render()
+    {
+        return view('livewire.edit-form');
+    }
+}
---
+++ resources/views/livewire/edit-form.blade.php
@@ -0,0 +1,84 @@
+<div class="px-6 py-8">
+    <main class="max-w-lg mx-auto mt-10">
+        <x-panel>
+            <h1 class="text-center font-bold text-xl">Editar Perfil</h1>
+
+            <!-- user input form  -->
+            <form wire:submit.prevent="update" class="mt-10">
+
+                <x-form.input name="name" type="text" :value="old('name', $name)" id="Nome" />
+                <x-form.input name="username" type="text" :value="old('username', $username)" id="Nome de Usuário" />
+
+                {{-- user profile pic --}}
+                <div class="flex items-center mt-6 mb-2">
+                    <div class="flex-1">
+
+                        <x-form.field>
+                            <x-form.label name="Foto de Perfil (Opcional)" />
+
+                            <input type="file" wire:model="profile_pic" class="border border-gray-400 p-2 w-full"
+                                name="profile_pic" id="{{ $profilePicId }}">
+
+                            @if ($profile_pic)
+                                <x-form.error name="profile_pic" />
+                            @endif
+                        </x-form.field>
+
+                    </div>
+
+                    <button type="button">
+                        <svg wire:click="removeSelectedProfilePic" viewBox="-5 -5 30 30" width="30" version="1.1"
+                            xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <g id="Page-1" stroke="none" stroke-width="1" fill="black" fill-rule="evenodd">
+                                <g id="icon-shape">
+                                    <polygon id="Combined-Shape"
+                                        points="10 8.58578644 2.92893219 1.51471863 1.51471863 2.92893219 8.58578644 10 1.51471863 17.0710678 2.92893219 18.4852814 10 11.4142136 17.0710678 18.4852814 18.4852814 17.0710678 11.4142136 10 18.4852814 2.92893219 17.0710678 1.51471863 10 8.58578644">
+                                    </polygon>
+                                </g>
+                            </g>
+                        </svg>
+                    </button>
+
+                    @if ($profile_pic)
+
+                        {{-- check to see if $profile_pic is a string because if it is, then it has already been stored into the storage public/storage folder --}}
+                        @if (is_string($profile_pic))
+                            <img src="{{ asset('storage/' . $profile_pic) }}" alt="Foto de Perfil"
+                                class="rounded-xl ml-6 square" width="100">
+                        @else
+                            @php
+                                try {
+                                    $tempUrl = $profile_pic->temporaryUrl();
+                                } catch (\Exception $e) {
+                                    $tempUrl = '/images/generic_profile_pic.png';
+                                }
+                            @endphp
+
+                            <img src="{{ $tempUrl }}" alt="Foto de Perfil" class="rounded-xl ml-6 square"
+                                width="100">
+                        @endif
+                    @else
+                        <img src="/images/generic_profile_pic.png" alt="Foto de Perfil" class="rounded-xl ml-6 square"
+                            width="100">
+                    @endif
+
+                </div>
+                {{-- end user profile pic --}}
+
+                <x-form.button>
+                    <svg wire:loading wire:target="update" class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                        xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor"
+                            stroke-width="4"></circle>
+                        <path class="opacity-75" fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                        </path>
+                    </svg>
+
+                    <span>Editar</span>
+                </x-form.button>
+
+            </form>
+        </x-panel>
+    </main>
+</div>

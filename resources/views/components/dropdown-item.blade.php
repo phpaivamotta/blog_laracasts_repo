ece6292801f9292daf47b0be16d9ca36643b71df
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-08-04 12:17:08 -0300

    increased dropdown item size and flash message size

--- resources/views/components/dropdown-item.blade.php
+++ resources/views/components/dropdown-item.blade.php
@@ -1,12 +1,11 @@
 @props(['active' => false])
 
 @php 
-    $classes = 'block text-left px-3 text-3xl lg:text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white';
+    $classes = 'block text-left py-5 lg:py-0 px-3 text-4xl lg:text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white';
 
-    if($active){
+    if ( $active ) {
         $classes = $classes . ' bg-blue-500 text-white';
     }
-    
 @endphp
 
 <a {{ $attributes(['class' => $classes ]) }}> {{ $slot }} </a>

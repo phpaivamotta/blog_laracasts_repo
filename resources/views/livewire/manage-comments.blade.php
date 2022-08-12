59016214313338c0daa1a0e0c73684bde7c40532
Pedro Henrique de Paiva Motta <phpaivamotta@gmail.com>
2022-08-12 12:20:04 -0300

    translated comment posted at statement

--- resources/views/livewire/manage-comments.blade.php
+++ resources/views/livewire/manage-comments.blade.php
@@ -19,7 +19,7 @@
                     <header class="mb-4">
                         <h3 class="font-bold">{{ $comment->author->username }}</h3>
                         <p class="text-xs">
-                            Posted
+                            Postado
                             <time>{{ $comment->created_at->diffForHumans() }}</time>
                         </p>
                     </header>
